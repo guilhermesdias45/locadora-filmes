299477548c0ea9da381aaf0b5319f079087b7e28
guilhermesdias45 <guilherme.sdias@sptech.school>
2026-04-30 15:58:04 -0300

    Initial commit

---
+++ locadora-filmes/src/main/java/school/sptech/locadora_filmes/LocadoraFilmesApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.locadora_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraFilmesApplication.class, args);
+	}
+
+}
---
+++ locadora-filmes/src/test/java/school/sptech/locadora_filmes/LocadoraFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.locadora_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
